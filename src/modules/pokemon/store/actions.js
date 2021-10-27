055fcd77c207d246fd4bff9967f9140365979f1b
Samuel Mena <samuel23mena@gmail.com>
2021-10-27 08:49:27 -0600

    :sparkles: Cambiando metodo en actions para cargar los pokemones

--- src/modules/pokemon/store/actions.js
+++ src/modules/pokemon/store/actions.js
@@ -1,7 +1,7 @@
 import { pokemonApi } from "@/api/pokemon"
 
 export const loadPokemons = async ({commit}) => {
-  const { data } = await pokemonApi.get('/pokemon?limit=150')
+  // const { data } = await pokemonApi.get('/pokemon?limit=150')
   // commit('setPokemons', data.results)
 
   /**
@@ -9,25 +9,24 @@ export const loadPokemons = async ({commit}) => {
    * informacion de cada pokemon, incluyendo el type
    * y demas informacion valiosa
    * 
-   * Haciendo uso de Promise
+   * Haciendo uso de un ciclo for
    */
-  const tempPokemons = []
+  const pokemonsList = []
   for (let i = 1; i <= 150; i++) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    tempPokemons.push(
-      await pokemonApi.get(url)
-        .then(({data}) => {
-          const pokemon = {
-            name: data.name,
-            id: data.id,
-            image: data.sprites['front_default'],
-            type: data.types
-          }
-        })
-    );
+    await pokemonApi.get(url)
+      .then(({data}) => {
+        const pokemon = {
+          name: data.name,
+          id: data.id,
+          image: data.sprites['front_default'],
+          type: data.types
+        }
+        pokemonsList.push(pokemon)
+      })
   }
-  console.log(tempPokemons);
-  commit('setPokemons', tempPokemons)
+
+  commit('setPokemons', pokemonsList)
 }
 
 export const loadPokemon = async ({commit}, id) => {
